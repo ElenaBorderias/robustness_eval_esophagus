334fa5a3e7c14676750cac619af9a6d6d42b9ad0
Luis Santos <luis.santos@lab900.com>
2021-03-11 19:54:30 +0100

    chore: remove shit exception handling

--- robustness_eval_esophagus.py
+++ robustness_eval_esophagus.py
@@ -51,125 +51,121 @@ def worst_dose(doses, roi_type, dose_calculation):
         return max(calculated_doses)
 
 
+
+###############################################################################
+###############################################################################
+
+plan = case.TreatmentPlans[plan_name]
+beam_set = plan.BeamSets[0]
+
+rss_group_name = "ROB_EVAL_SE_RE"
+
+# Run robustness evaluation range error (RE) and setup error (SE)
 try:
-    ###############################################################################
-    ###############################################################################
-
-    plan = case.TreatmentPlans[plan_name]
-    beam_set = plan.BeamSets[0]
-
-    rss_group_name = "ROB_EVAL_SE_RE"
-
-    # Run robustness evaluation range error (RE) and setup error (SE)
-    try:
-        beam_set.CreateRadiationSetScenarioGroup(Name=rss_group_name,
-                                                 UseIsotropicPositionUncertainty=isotropic_pos_uncertainty,
-                                                 PositionUncertaintySuperior=setup_error,
-                                                 PositionUncertaintyInferior=setup_error,
-                                                 PositionUncertaintyPosterior=setup_error,
-                                                 PositionUncertaintyAnterior=setup_error,
-                                                 PositionUncertaintyLeft=setup_error,
-                                                 PositionUncertaintyRight=setup_error,
-                                                 PositionUncertaintyFormation="AxesAndDiagonalEndPoints",
-                                                 PositionUncertaintyList=None,
-                                                 DensityUncertainty=range_error,
-                                                 NumberOfDensityDiscretizationPoints=nb_density_discretization_points,
-                                                 ComputeScenarioDosesAfterGroupCreation=True)
-    except Exception:
-        print("Scenario Group" + rss_group_name + " exists already")
-
-    # Reading a dose
-    nominal_dose = plan.PlanOptimizations[0].TreatmentCourseSource.TotalDose
-    patient_model = case.PatientModel.StructureSets[0]
-
-    ## Storing Results
-    results = {}
-
-    print("Reading RSS Groups")
-    rssGroups = case.TreatmentDelivery.RadiationSetScenariosGroups
-    # correct group rss.Name == rss_group_name && rss.ReferencedRaditionSet.DicomPlanLabel == plan_name
-    rssGroup = (filter(lambda rss: rss.Name == rss_group_name and rss.ReferencedRaditionSet.DicomPlanLabel == plan_name,
-                       rssGroups))[0]
-
-    if rssGroup is not None:
-        print("Found corresponding RSS group " + rssGroup.Name)
-
-    discrete_doses = rssGroup.DiscreteFractionDoseScenarios + [nominal_dose]
-
-    print("Finished RSS Groups")
-
-    # Get statistics based ROIs
-    print("Reading Dose Statistics ROIs")
-    dose_statistics_rois = [
-        {
-            'label': 'CTV_45',
-            'metric': 'Dmean',
-            'name': 'MT_CTVt_4500',
-            'doseType': 'Average',
-            'roi_type': 'target',
-        },
-        {
-            'label': 'iCTV_45',
-            'metric': 'Dmean',
-            'name': 'MT_iCTVt_4500',
-            'doseType': 'Average',
-            'roi_type': 'target',
-        }
-    ]
-
-    for dose_stat_roi in dose_statistics_rois:
-        results[get_key(dose_stat_roi) + '_nominal'] = get_dose_statistic(nominal_dose, dose_stat_roi['name'],
-                                                                          dose_stat_roi['doseType'])
-        results[get_key(dose_stat_roi) + '_worst'] = worst_dose(discrete_doses,
-                                                                dose_stat_roi['roi_type'],
-                                                                lambda dose: get_dose_statistic(dose,
-                                                                                                dose_stat_roi['name'],
-                                                                                                dose_stat_roi[
-                                                                                                    'doseType']))
-    print("Finished Dose Statistics ROIs")
-
-
-    # Get relative volume based ROIs
-    print("Reading Dose Relative Volume ROIs")
-    dose_relative_volume_rois = [
-        {
-            'label': 'iCTV_45',
-            'metric': 'V95',
-            'name': 'MT_iCTVt_4500',
-            'RelativeVolumes': [0.95],
-            'roi_type': "target",
-        },
-        {
-            'label': 'Spinal_Cord',
-            'metric': 'D0_05',
-            'name': 'MT_SpinalCanal',
-            'relativeVolumes': [get_relative_volume_roi_geometries(patient_model, 'SpinalCord', 0.05)],
-            'roi_type': "organ_at_risk",
-        },
-    ]
-
-    for dose_relative_volume_roi in dose_relative_volume_rois:
-        results[get_key(dose_relative_volume_roi) + '_nominal'] = get_dose_at_relative_volume(nominal_dose,
-                                                                                              dose_relative_volume_roi[
-                                                                                                  'name'],
-                                                                                              dose_relative_volume_roi[
-                                                                                                  'relativeVolumes'])
-        results[get_key(dose_relative_volume_roi) + '_worst'] = worst_dose(discrete_doses,
-                                                                           dose_relative_volume_roi['roi_type'],
-                                                                           lambda dose: get_dose_at_relative_volume(
-                                                                               dose,
-                                                                               dose_relative_volume_roi['name'],
-                                                                               dose_relative_volume_roi[
-                                                                                   'relativeVolumes']))
-    print("Finished Dose Relative Volume ROIs")
-
-    print("Writing results...")
-    output_path = "Z:\\"
-    with open('data.json', 'w') as f:
-        json.dump(results, f)
-    print("Written results!")
-    print("Done")
-
+    beam_set.CreateRadiationSetScenarioGroup(Name=rss_group_name,
+                                             UseIsotropicPositionUncertainty=isotropic_pos_uncertainty,
+                                             PositionUncertaintySuperior=setup_error,
+                                             PositionUncertaintyInferior=setup_error,
+                                             PositionUncertaintyPosterior=setup_error,
+                                             PositionUncertaintyAnterior=setup_error,
+                                             PositionUncertaintyLeft=setup_error,
+                                             PositionUncertaintyRight=setup_error,
+                                             PositionUncertaintyFormation="AxesAndDiagonalEndPoints",
+                                             PositionUncertaintyList=None,
+                                             DensityUncertainty=range_error,
+                                             NumberOfDensityDiscretizationPoints=nb_density_discretization_points,
+                                             ComputeScenarioDosesAfterGroupCreation=True)
 except Exception:
-    print("You broke shit.")
-    raise
+    print("Scenario Group" + rss_group_name + " exists already")
+
+# Reading a dose
+nominal_dose = plan.PlanOptimizations[0].TreatmentCourseSource.TotalDose
+patient_model = case.PatientModel.StructureSets[0]
+
+## Storing Results
+results = {}
+
+print("Reading RSS Groups")
+rssGroups = case.TreatmentDelivery.RadiationSetScenariosGroups
+# correct group rss.Name == rss_group_name && rss.ReferencedRaditionSet.DicomPlanLabel == plan_name
+rssGroup = (filter(lambda rss: rss.Name == rss_group_name and rss.ReferencedRaditionSet.DicomPlanLabel == plan_name,
+                   rssGroups))[0]
+
+if rssGroup is not None:
+    print("Found corresponding RSS group " + rssGroup.Name)
+
+discrete_doses = rssGroup.DiscreteFractionDoseScenarios + [nominal_dose]
+
+print("Finished RSS Groups")
+
+# Get statistics based ROIs
+print("Reading Dose Statistics ROIs")
+dose_statistics_rois = [
+    {
+        'label': 'CTV_45',
+        'metric': 'Dmean',
+        'name': 'MT_CTVt_4500',
+        'doseType': 'Average',
+        'roi_type': 'target',
+    },
+    {
+        'label': 'iCTV_45',
+        'metric': 'Dmean',
+        'name': 'MT_iCTVt_4500',
+        'doseType': 'Average',
+        'roi_type': 'target',
+    }
+]
+
+for dose_stat_roi in dose_statistics_rois:
+    results[get_key(dose_stat_roi) + '_nominal'] = get_dose_statistic(nominal_dose, dose_stat_roi['name'],
+                                                                      dose_stat_roi['doseType'])
+    results[get_key(dose_stat_roi) + '_worst'] = worst_dose(discrete_doses,
+                                                            dose_stat_roi['roi_type'],
+                                                            lambda dose: get_dose_statistic(dose,
+                                                                                            dose_stat_roi['name'],
+                                                                                            dose_stat_roi[
+                                                                                                'doseType']))
+print("Finished Dose Statistics ROIs")
+
+
+# Get relative volume based ROIs
+print("Reading Dose Relative Volume ROIs")
+dose_relative_volume_rois = [
+    {
+        'label': 'iCTV_45',
+        'metric': 'V95',
+        'name': 'MT_iCTVt_4500',
+        'RelativeVolumes': [0.95],
+        'roi_type': "target",
+    },
+    {
+        'label': 'Spinal_Cord',
+        'metric': 'D0_05',
+        'name': 'MT_SpinalCanal',
+        'relativeVolumes': [get_relative_volume_roi_geometries(patient_model, 'SpinalCord', 0.05)],
+        'roi_type': "organ_at_risk",
+    },
+]
+
+for dose_relative_volume_roi in dose_relative_volume_rois:
+    results[get_key(dose_relative_volume_roi) + '_nominal'] = get_dose_at_relative_volume(nominal_dose,
+                                                                                          dose_relative_volume_roi[
+                                                                                              'name'],
+                                                                                          dose_relative_volume_roi[
+                                                                                              'relativeVolumes'])
+    results[get_key(dose_relative_volume_roi) + '_worst'] = worst_dose(discrete_doses,
+                                                                       dose_relative_volume_roi['roi_type'],
+                                                                       lambda dose: get_dose_at_relative_volume(
+                                                                           dose,
+                                                                           dose_relative_volume_roi['name'],
+                                                                           dose_relative_volume_roi[
+                                                                               'relativeVolumes']))
+print("Finished Dose Relative Volume ROIs")
+
+print("Writing results...")
+output_path = "Z:\\"
+with open('data.json', 'w') as f:
+    json.dump(results, f)
+print("Written results!")
+print("Done")
