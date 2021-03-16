ae0e57de08c13a6ea7a997d3c20c949bdc94d584
ElenaBorderias <elena.borderias@uclouvain.be>
2021-03-16 13:43:22 +0100

    fix relative volumes at dose function

--- robustness_eval_esophagus.py
+++ robustness_eval_esophagus.py
@@ -46,9 +46,8 @@ def get_dose_at_relative_volume(dose, roi_name, relative_volume):
 
 def get_relative_volume_at_dose_value(dose, roi_name, dose_value):
     return round(float(
-        dose.GetDoseAtRelativeVolumes(RoiName=roi_name,
-                                      DoseValues=[dose_value])[0]), 2)
-        
+        dose.GetRelativeVolumeAtDoseValues(RoiName=roi_name,
+                                      DoseValues=[dose_value])[0]), 2)        
 
 def worst_dose(calculated_doses, roi_type):
     if roi_type == "target":
